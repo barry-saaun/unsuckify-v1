3da97d5ebff6c920164d1d495c7f6d7c74fea458
Barry Saaun <panhaocr@gmail.com>
2025-06-03 16:52:30 +1000

    (ui): added ui components for hero section of the homnepage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,33 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+
 function Dashboard() {
-  return <div>Hello from Dashboard</div>;
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <div className="mx-10 space-y-5 py-10">
+        <h1 className="text-3xl font-bold tracking-tight md:text-4xl">
+          Your Playlists
+        </h1>
+        <Tabs defaultValue="my-playlists">
+          <TabsList>
+            <TabsTrigger value="my-playlists" className="font-bold">
+              My Playlists
+            </TabsTrigger>
+            <TabsTrigger value="public-playlist" className="font-bold">
+              Public Playlist
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="my-playlists">
+            {/* <MyPlaylistsTabContent /> */}
+            <h1>Tab 1</h1>
+          </TabsContent>
+          <TabsContent value="public-playlist">
+            {/* <PublicPlaylistTabContent /> */}
+            <h1>Tab 2</h1>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
---
+++ src/components/ui/hero-highlight.tsx
@@ -0,0 +1,98 @@
+"use client";
+import { cn } from "@/lib/utils";
+import { useMotionValue, motion, useMotionTemplate } from "framer-motion";
+import React from "react";
+
+export const HeroHighlight = ({
+  children,
+  className,
+  containerClassName,
+}: {
+  children: React.ReactNode;
+  className?: string;
+  containerClassName?: string;
+}) => {
+  const mouseX = useMotionValue(0);
+  const mouseY = useMotionValue(0);
+
+  function handleMouseMove({
+    currentTarget,
+    clientX,
+    clientY,
+  }: React.MouseEvent<HTMLDivElement>) {
+    if (!currentTarget) return;
+    const { left, top } = currentTarget.getBoundingClientRect();
+
+    mouseX.set(clientX - left);
+    mouseY.set(clientY - top);
+  }
+  return (
+    <div
+      className={cn(
+        "group relative flex h-[40rem] w-full items-center justify-center bg-white dark:bg-black",
+        containerClassName,
+      )}
+      onMouseMove={handleMouseMove}
+    >
+      <div className="bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800 pointer-events-none absolute inset-0" />
+      <motion.div
+        className="bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500 pointer-events-none absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
+        style={{
+          WebkitMaskImage: useMotionTemplate`
+            radial-gradient(
+              200px circle at ${mouseX}px ${mouseY}px,
+              black 0%,
+              transparent 100%
+            )
+          `,
+          maskImage: useMotionTemplate`
+            radial-gradient(
+              200px circle at ${mouseX}px ${mouseY}px,
+              black 0%,
+              transparent 100%
+            )
+          `,
+        }}
+      />
+
+      <div className={cn("relative z-20", className)}>{children}</div>
+    </div>
+  );
+};
+
+export const Highlight = ({
+  children,
+  isBlock = false,
+  className,
+}: {
+  children: React.ReactNode;
+  isBlock?: boolean;
+  className?: string;
+}) => {
+  return (
+    <motion.span
+      initial={{
+        backgroundSize: "0% 100%",
+      }}
+      animate={{
+        backgroundSize: "100% 100%",
+      }}
+      transition={{
+        duration: 2,
+        ease: "linear",
+        delay: 0.5,
+      }}
+      style={{
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "left center",
+        display: isBlock ? "inline-block" : "inline",
+      }}
+      className={cn(
+        `relative inline-block rounded-lg bg-gradient-to-r from-green-400 to-indigo-400 px-1 pb-1 dark:from-green-500 dark:to-indigo-600`,
+        className,
+      )}
+    >
+      {children}
+    </motion.span>
+  );
+};
