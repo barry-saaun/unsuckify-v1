d700a7e6379da8096e6852bc61e7696a58f0f4de
Barry Saaun <panhaocr@gmail.com>
2025-06-06 19:08:17 +1000

    (misc): added spotify images protocol and hostname config

--- next.config.js
+++ next.config.js
@@ -5,6 +5,31 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "i.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "image-cdn-fa.spotifycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "mosaic.scdn.co",
+      },
+      {
+        protocol: "https",
+        hostname: "image-cdn-ak.spotifycdn.com",
+      },
+    ],
+  },
+};
 
 export default config;
