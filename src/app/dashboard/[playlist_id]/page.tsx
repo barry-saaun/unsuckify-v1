55435b33c2f4818bfe8b3c321bd317bd65e0ed1a
Barry Saaun <panhaocr@gmail.com>
2025-06-27 14:04:57 +1000

    params is async in nextjs 15

--- src/app/dashboard/[playlist_id]/page.tsx
+++ src/app/dashboard/[playlist_id]/page.tsx
@@ -1,9 +1,9 @@
 import { PlaylistContent } from "~/components/playlist-content-dashboard-server";
 
-type ParamsType = { params: { playlist_id: string } };
+type ParamsType = { params: Promise<{ playlist_id: string }> };
 
-function PlaylistContentDashboard({ params }: ParamsType) {
-  const playlist_id = params.playlist_id;
+async function PlaylistContentDashboard({ params }: ParamsType) {
+  const playlist_id = (await params).playlist_id;
 
   return (
     <PlaylistContent params={{ playlist_id }} searchParams={{ undefined }} />
