b0c592392437ea06f8ed0f8c11db42307023667b
Barry Saaun <panhaocr@gmail.com>
2025-06-30 14:44:59 +1000

    WIP: temp  changes

--- src/app/dashboard/[playlist_id]/page.tsx
+++ src/app/dashboard/[playlist_id]/page.tsx
@@ -3,8 +3,10 @@ import { useParams } from "next/navigation";
 import { api } from "~/trpc/react";
 import { skipToken } from "@tanstack/react-query";
 import ErrorScreen from "~/components/error-screen";
+import { useEffect, useRef } from "react";
+import type { TRecommendedTracks } from "~/types";
 
-export default function PlaylistConetnt() {
+export default function PlaylistContent() {
   const params = useParams<{ playlist_id: string }>();
 
   const playlist_id = params.playlist_id;
@@ -21,13 +23,30 @@ export default function PlaylistConetnt() {
   );
 
   const {
-    data: rec_data,
+    data: rec_tracks,
     isLoading: isLoadingRecommendations,
     error: recommendationsError,
   } = api.track.getRecommendations.useQuery(playlistData ?? skipToken, {
     enabled: !!playlistData,
     staleTime: 86400 * 1000,
   });
+
+  const lastSentRef = useRef<TRecommendedTracks>(null);
+
+  const { mutate } = api.track.pushRecommendations.useMutation();
+
+  const userId = "31y3qs5dnfs6cyuivogbn5fgl3au";
+
+  useEffect(() => {
+    if (!rec_tracks) return;
+
+    if (JSON.stringify(lastSentRef.current) === JSON.stringify(rec_tracks)) {
+      return;
+    }
+
+    mutate({ playlistId: playlist_id, userId, recommendations: rec_tracks });
+    lastSentRef.current = rec_tracks;
+  }, [rec_tracks, playlist_id, mutate]);
 
   // Handle errors for either query
   if (playlistError) {
@@ -52,9 +71,9 @@ export default function PlaylistConetnt() {
     return <ErrorScreen message="No playlist data found." />;
   }
 
-  if (!rec_data) {
+  if (!rec_tracks) {
     console.error("No recommendation data found.");
   }
 
-  return <div>{JSON.stringify(rec_data)}</div>;
+  return <div>{JSON.stringify(rec_tracks)}</div>;
 }
