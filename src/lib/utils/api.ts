7f209bd5d89801fbf0e505b49c17e74d49a63676
Barry Saaun <panhaocr@gmail.com>
2025-07-29 15:46:08 +1000

    (env): added next public base url

--- src/lib/utils/api.ts
+++ src/lib/utils/api.ts
@@ -1,5 +1,11 @@
 export function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin;
+
+  // Use custom base URL if set
+  if (process.env.NEXT_PUBLIC_BASE_URL) {
+    return process.env.NEXT_PUBLIC_BASE_URL;
+  }
+
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
