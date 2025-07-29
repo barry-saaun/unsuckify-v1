73fbeda79e53fdcb1992b5a66e4811b1b0e2ee6e
Barry Saaun <panhaocr@gmail.com>
2025-07-29 15:14:23 +1000

    (env): removed unnecessary vars

--- src/env.js
+++ src/env.js
@@ -22,9 +22,9 @@ export const env = createEnv({
    */
   client: {
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
-    NEXT_PUBLIC_SPOTIFY_REDIRECT_URI: z
-      .string()
-      .url("NEXT_PUBLIC_SPOTIFY_REDIRECT_URI must be a valid URL"),
+    // NEXT_PUBLIC_SPOTIFY_REDIRECT_URI: z
+    //   .string()
+    //   .url("NEXT_PUBLIC_SPOTIFY_REDIRECT_URI must be a valid URL"),
   },
 
   /**
@@ -35,8 +35,8 @@ export const env = createEnv({
     NODE_ENV: process.env.NODE_ENV,
     SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
     SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
-    NEXT_PUBLIC_SPOTIFY_REDIRECT_URI:
-      process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI,
+    // NEXT_PUBLIC_SPOTIFY_REDIRECT_URI:
+    //   process.env.NEXT_PUBLIC_SPOTIFY_REDIRECT_URI,
 
     DATABASE_URL: process.env.DATABASE_URL,
   },
