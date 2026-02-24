c10569102599125fcbae795aa2367f8c65baec60
Barry Saaun <panhaocr@gmail.com>
2026-02-25 09:45:17 +1100

    (feat) added inngest clients, functions, and api endpoints

---
+++ src/app/api/inngest/types.ts
@@ -0,0 +1,23 @@
+export type InngestEvents = {
+  "music/playlist.embed.requested": {
+    data: {
+      userId: string;
+      playlist: Array<{ artist: string; track: string }>;
+    };
+  };
+  "music/song.embed.requested": {
+    data: {
+      userId: string;
+      artist: string;
+      track: string;
+    };
+  };
+
+  "music/song.embed.completed": {
+    data: {
+      userId: string;
+      songKey: string;
+      status: "skipped" | "updated";
+    };
+  };
+};
